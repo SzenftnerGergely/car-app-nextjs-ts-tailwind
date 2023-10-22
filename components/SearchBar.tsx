e0424a54c5d002cc518b22af69f7d32e38a91847
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-22 09:50:10 +0200

    searchbar component updated with searchbutton

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,20 @@
 
 import {useState} from 'react'
 import SearchManufacturer from './SearchManufacturer'
+import Image from "next/image"
+
+const SearchButton = ({otherClasses}: {otherClasses: string}) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image 
+      src="/magnifying-glass.svg"
+      alt='magnifying glass'
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+)
+
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("")
@@ -16,6 +30,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses='sm:hidden' />
       </div>
     </form>
   )
